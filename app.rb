ba03b658b22e43ea9f3e43de45ed479d0d9605d1
Melissa Real <melissa.real@hotmail.com>
2018-11-04 16:40:58 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    # some code to get the medicines and render the correct HTML file
+end
+
+get '/hometown' do
+    # some code to get the medicines and render the correct HTML file
+end
+
+get '/favorite-song' do
+    # some code to get the medicines and render the correct HTML file
+end
+
+
 end
