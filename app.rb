d19c85c43ab7b59ead82205fa1056439a92ccbce
Melissa Real <melissa.real@hotmail.com>
2018-11-04 16:42:56 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,15 +3,15 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/name' do
-    puts "My name is Mr.potato"
+     "My name is Mr.potato"
   end
 
   get '/hometown' do
-    puts "My hometown is toy story"
+     "My hometown is toy story"
   end
 
   get '/favorite-song' do
-    puts "My favourite-song is yeepy-ya-yo"
+     "My favourite-song is yeepy-ya-yo"
   end
 
 end
